3fb5b1b561177d4cd01b5cde281093461a7042f6
Erik Englund <erik.englund@axiosengineering.com>
2014-03-11 18:07:56 -0400

    Added a init method to ensure Jacorb is configured correctly.

--- build02/plugins/org.jacorb.eclipse/src/org/jacorb/eclipse/JacorbEclipseActivator.java
+++ build02/plugins/org.jacorb.eclipse/src/org/jacorb/eclipse/JacorbEclipseActivator.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import java.util.logging.LogManager;
 
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
@@ -18,18 +19,33 @@ public class JacorbEclipseActivator extends Plugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.jacorb.eclipse";
+	private static JacorbEclipseActivator instance;
+	private boolean init;
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
 	public void start(BundleContext bundleContext) throws Exception {
 		super.start(bundleContext);
-		
+		JacorbEclipseActivator.instance = this;
+
+	}
+
+	public static JacorbEclipseActivator getDefault() {
+		return JacorbEclipseActivator.instance;
+	}
+
+	public void init() {
+		if (init) {
+			return;
+		}
+		init = true;
 		// Ensure Jacorb and Java Logger are configured correctly
-		configureJavaLogger(bundleContext);
-		configureJacorb(bundleContext);
+		configureJavaLogger(getBundle().getBundleContext());
+		configureJacorb(getBundle().getBundleContext());
 	}
 
 	private void configureJacorb(BundleContext context) {
@@ -47,7 +63,9 @@ public class JacorbEclipseActivator extends Plugin {
 			propUrl = Platform.getConfigurationLocation().getDataArea("jacorb.properties");
 		} catch (IOException e) {
 			propUrl = FileLocator.find(context.getBundle(), new Path("jacorb.properties"), null);
-			getLog().log(new Status(Status.WARNING, PLUGIN_ID, "Failed to find configure/jacorb.config.dir, using default configuration: " + propUrl, e));
+			getLog().log(
+				new Status(IStatus.WARNING, JacorbEclipseActivator.PLUGIN_ID, "Failed to find configure/jacorb.config.dir, using default configuration: "
+					+ propUrl, e));
 		} finally {
 			if (propUrl != null) {
 				URL fileUrl;
@@ -56,12 +74,18 @@ public class JacorbEclipseActivator extends Plugin {
 					File file = new File(fileUrl.toURI());
 					System.setProperty("jacorb.config.dir", file.getParentFile().getAbsolutePath());
 				} catch (IOException e) {
-					getLog().log(new Status(Status.WARNING, PLUGIN_ID, "Failed to configure jacorb.config.dir location.  CORBA operations may not work.", e));
+					getLog().log(
+						new Status(IStatus.WARNING, JacorbEclipseActivator.PLUGIN_ID,
+							"Failed to configure jacorb.config.dir location.  CORBA operations may not work.", e));
 				} catch (URISyntaxException e) {
-					getLog().log(new Status(Status.WARNING, PLUGIN_ID, "Failed to configure jacorb.config.dir location.  CORBA operations may not work.", e));
+					getLog().log(
+						new Status(IStatus.WARNING, JacorbEclipseActivator.PLUGIN_ID,
+							"Failed to configure jacorb.config.dir location.  CORBA operations may not work.", e));
 				}
 			} else {
-				getLog().log(new Status(Status.WARNING, PLUGIN_ID, "Failed to configure jacorb.config.dir location.  CORBA operations may not work.", null));
+				getLog().log(
+					new Status(IStatus.WARNING, JacorbEclipseActivator.PLUGIN_ID,
+						"Failed to configure jacorb.config.dir location.  CORBA operations may not work.", null));
 			}
 		}
 	}
@@ -69,9 +93,9 @@ public class JacorbEclipseActivator extends Plugin {
 	private void setProperties() {
 		System.setProperty("com.sun.CORBA.transport.ORBUseNIOSelectToWait", "false");
 		System.setProperty("java.net.preferIPv4Stack", "true");
-		System.setProperty("org.omg.CORBA.ORBClass","org.jacorb.orb.ORB");
-		System.setProperty("org.omg.CORBA.ORBSingletonClass","org.jacorb.orb.ORBSingleton");
-		System.setProperty("org.omg.PortableInterceptor.ORBInitializerClass.standard_init","org.jacorb.orb.standardInterceptors.IORInterceptorInitializer");
+		System.setProperty("org.omg.CORBA.ORBClass", "org.jacorb.orb.ORB");
+		System.setProperty("org.omg.CORBA.ORBSingletonClass", "org.jacorb.orb.ORBSingleton");
+		System.setProperty("org.omg.PortableInterceptor.ORBInitializerClass.standard_init", "org.jacorb.orb.standardInterceptors.IORInterceptorInitializer");
 	}
 
 	/**
@@ -89,7 +113,7 @@ public class JacorbEclipseActivator extends Plugin {
 				return;
 			}
 		}
-		
+
 		InputStream test = null;
 		InputStream logInputStream = null;
 		try {
@@ -126,11 +150,13 @@ public class JacorbEclipseActivator extends Plugin {
 
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
 	public void stop(BundleContext bundleContext) throws Exception {
 		super.stop(bundleContext);
+		JacorbEclipseActivator.instance = null;
 	}
 
 }
