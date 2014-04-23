e1e59702bfa766d3e434b4a30973db2b18304336
Erik Englund <erik.englund@axiosengineering.com>
2014-04-22 22:11:37 -0400

    refs #2036 Add code to deal with Java version update incompatible changes.

--- build02/plugins/org.jacorb.eclipse/src/org/jacorb/eclipse/JacorbEclipseActivator.java
+++ build02/plugins/org.jacorb.eclipse/src/org/jacorb/eclipse/JacorbEclipseActivator.java
@@ -1,6 +1,11 @@
 package org.jacorb.eclipse;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -11,6 +16,10 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.Plugin;
+import org.eclipse.core.runtime.adaptor.EclipseStarter;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.osgi.service.datalocation.Location;
+import org.omg.CORBA.ORB;
 import org.osgi.framework.BundleContext;
 
 public class JacorbEclipseActivator extends Plugin {
@@ -19,6 +28,7 @@ public class JacorbEclipseActivator extends Plugin {
 	public static final String PLUGIN_ID = "org.jacorb.eclipse";
 	private static JacorbEclipseActivator instance;
 	private boolean init;
+	private boolean postInit;
 
 	/*
 	 * (non-Javadoc)
@@ -32,11 +42,148 @@ public class JacorbEclipseActivator extends Plugin {
 		init();
 	}
 
+	private void postInit() {
+		if (postInit) {
+			return;
+		}
+		postInit = true;
+		String version = System.getProperty("java.version");
+		if (version.startsWith("1.7.0_")) {
+			int minorVersion = Integer.parseInt(version.substring(6));
+			if (minorVersion < 55) {
+				return;
+			}
+		} else if (version.startsWith("1.8.0_")) {
+			// PASS
+			// Always do this for 1.8
+		} else if (version.startsWith("1.6.0_")) {
+			int minorVersion = Integer.parseInt(version.substring(6));
+			if (minorVersion < 30) {
+				return;
+			}
+		}
+		try {
+			ORB.init();
+		} catch (Throwable exception) {
+			Location installLocation = Platform.getInstallLocation();
+			boolean autoConfigured = false;
+			boolean shouldConfigure = false;
+			Exception configureException = null;
+			String jacorbLine = "-Djava.endorsed.dirs=<Eclipse Dir>/jacorb/lib";
+			if (installLocation != null) {
+				URL homeUrl = installLocation.getURL();
+				File homeFile = new File(homeUrl.getPath());
+				if (homeFile.exists()) {
+					File jacorbDir = null;
+					try {
+						jacorbDir = new File(FileLocator.toFileURL(FileLocator.find(Platform.getBundle("org.jacorb"), new Path("jars"), null)).getPath());
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+					
+					if (jacorbDir != null && jacorbDir.exists()) {
+						jacorbLine = "-Djava.endorsed.dirs=" + jacorbDir.getAbsolutePath();
+						File iniFile = new File(homeFile, "eclipse.ini");
+						if (!iniFile.exists()) {
+							File[] files = homeFile.listFiles(new FilenameFilter() {
+
+								@Override
+								public boolean accept(File dir, String name) {
+									return name.endsWith(".ini");
+								}
+							});
+							if (files != null && files.length > 0) {
+								iniFile = files[0];
+							}
+						}
+						if (iniFile.exists()) {
+							shouldConfigure = true;
+							StringBuilder buffer = new StringBuilder();
+							BufferedReader reader = null;
+							BufferedWriter writer = null;
+							boolean foundConfig = false;
+							try {
+								reader = new BufferedReader(new FileReader(iniFile));
+								for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+									if (line.trim().startsWith("-Djava.endorsed.dirs")) {
+										if (jacorbLine.equals(line)) {
+											exception.printStackTrace();
+											System.exit(-1);
+										}
+										buffer.append(jacorbLine);
+										foundConfig = true;
+									} else if (!line.trim().isEmpty()) {
+										buffer.append(line);
+									} else {
+										continue;
+									}
+									buffer.append("\n");
+								}
+								if (!foundConfig) {
+									buffer.append(jacorbLine);
+									buffer.append("\n");
+								}
+								writer = new BufferedWriter(new FileWriter(iniFile));
+								writer.write(buffer.toString());
+								writer.flush();
+								autoConfigured = true;
+							} catch (IOException e) {
+								configureException = e;
+							} finally {
+								if (reader != null) {
+									IOUtils.closeQuietly(reader);
+								}
+								if (writer != null) {
+									IOUtils.closeQuietly(writer);
+								}
+							}
+						}
+					}
+				}
+			}
+
+			
+			if (shouldConfigure) {
+				if (!autoConfigured) {
+					String msg = "Please add the following to your eclipse.ini:\n\t" + jacorbLine;
+//					if (Display.getCurrent() != null) {
+//						if (ErrorDialog.openError(Display.getCurrent().getActiveShell(), "Jacorb Configuration", msg, new Status(Status.ERROR, PLUGIN_ID,
+//							"Unable to automatically update Eclipse.ini Jacorb Configuration.", configureException)) == Dialog.OK) {
+//							System.exit(0);
+//						}
+//					}
+					
+					System.err.println(msg);
+					if (configureException != null) {
+						configureException.printStackTrace();
+					}
+					System.exit(-1);
+					
+				} else {
+					System.err.println("Updated Jacorb configuration. Please restart the application to take affect.");
+					System.exit(IApplication.EXIT_OK);
+				}
+			} else {
+				String msg = "Please add the following to your vm args:\n\t" + jacorbLine;
+				System.err.println(msg);
+//				if (Display.getCurrent() != null) {
+//					if (ErrorDialog.openError(Display.getCurrent().getActiveShell(), "Jacorb Configuration", msg, new Status(Status.ERROR, PLUGIN_ID,
+//						"Unable to instantiate Jacorb ORB.", exception)) == Dialog.OK) {
+//						
+//					};
+//				}
+				System.exit(-1);
+			}
+		}
+
+	}
+
 	public static JacorbEclipseActivator getDefault() {
 		return JacorbEclipseActivator.instance;
 	}
 
-	public void init() {
+	public synchronized void init() {
 		if (init) {
 			return;
 		}
@@ -44,6 +191,7 @@ public class JacorbEclipseActivator extends Plugin {
 		// Ensure Jacorb and Java Logger are configured correctly
 		configureJavaLogger(getBundle().getBundleContext());
 		configureJacorb(getBundle().getBundleContext());
+		postInit();
 	}
 
 	private void configureJacorb(BundleContext context) {
@@ -83,7 +231,7 @@ public class JacorbEclipseActivator extends Plugin {
 				}
 			} else {
 				// TODO Log this to the trace logger
-			
+
 			}
 		}
 	}
